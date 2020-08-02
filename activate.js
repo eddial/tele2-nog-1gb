7816ffe9b0fddc5c7518c7adae720db57372e60b
Badr Eddial <badr.eddial@gmail.com>
2020-08-02 13:34:11 +0200

    Update activate.js

--- activate.js
+++ activate.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 async function main(credentials) {
     
     console.log('Launching browser window');
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36');
